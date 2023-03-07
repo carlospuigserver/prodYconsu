b83265dd55cb4101e75aa4593e4148b092f0ec36
carlospuigserver <cpuiggut@myuax.com>
2023-03-07 13:31:55 +0100

    acabado

--- cajas.py
+++ cajas.py
@@ -14,5 +14,38 @@ def empacarProducto(producto,caja):
     print("Preparando", caja,"hay", producto,"productos, empaquetamos 10, sobran", producto-10)
 
 def productora():
+    global numero_cajas
+    while True:
+        if numero_cajas>10:
+            break
+        caja=produccionCaja()
+        try:
+            colaCajas.put(caja,block=False)
+            print("Se está produciendo la  ",caja)
+            numero_cajas+=1
+        except queue.Full:
+            print("La cola está llena, tendrás que esperar")
+            time.sleep(2)
 
 
+def empleados():
+    while True:
+        try:
+            caja=colaCajas.get(block=False)
+            empacarProducto(15,caja) #15 es el número de productos que se van a empacar en cada caja
+        except queue.Empty:
+            print("La cola está vacía, tendrás que esperar")
+            time.sleep(2)
+
+
+
+# Creamos los hilos
+
+hiloProductora=threading.Thread(target=productora)
+hiloEmpleados=threading.Thread(target=empleados)
+
+hiloProductora.start()
+hiloEmpleados.start()
+
+hiloProductora.join()
+hiloEmpleados.join()
